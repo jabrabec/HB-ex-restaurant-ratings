241ea514e1777d1ed4bd0461851f7f6a0301489e
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 18:00:55 -0700

    add user choice loop

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -17,12 +17,31 @@ def print_ratings(filename):
             name, rating = line.strip().split(":")
             restaurant_ratings[name] = rating
 
-    user_add_name = raw_input("Please enter a restaurant name: ").title()
-    user_add_rating = raw_input("Please enter a restaurant rating: ")
+    while True:
+        print "\nDo you want to:"
+        print "1: See all current ratings?"
+        print "2: Add a new restaurant and rate it?"
+        print "Or 3: Quit?"
+        user_choice = raw_input("Choose one: ")
 
-    restaurant_ratings[user_add_name] = user_add_rating
+        if user_choice == '1':
+            for name, rating in sorted(restaurant_ratings.items()):
+                print "%s is rated at %s." % (name, rating)        
 
-    for name, rating in sorted(restaurant_ratings.items()):
-        print "%s is rated at %s." % (name, rating)
+        elif user_choice == '2':
+            user_add_name = raw_input("Please enter a restaurant name: ").title()
+            user_add_rating = raw_input("Please enter a restaurant rating: ")
+
+            restaurant_ratings[user_add_name] = user_add_rating
+
+            for name, rating in sorted(restaurant_ratings.items()):
+                print "%s is rated at %s." % (name, rating)
+
+        elif user_choice == '3':
+            print "Thanks!"
+            return
+
+        else:
+            print "Please enter 1, 2, or 3."
 
 print_ratings(filename)
